0f2ae5bf9429fd1b3baed5a32023ca80617fb51e
Anderson Dario <anderson@jp7.com.br>
2017-02-15 11:22:29 -0200

    Create ContactList component

---
+++ src/containers/ContactList.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class ContactList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Contacts</h1>
+        <table className="table table-striped table-responsive">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Phone</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.props.contactList.map(({ name, phone, email }) => (
+                  <tr>
+                    <td>{name}</td>
+                    <td>{phone}</td>
+                    <td>{email}</td>
+                  </tr>
+              ))
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  ...state.contacts
+});
+
+const mapDispatchToProps = dispatch => ({});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ContactList);
